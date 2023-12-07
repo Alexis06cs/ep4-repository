59e030f0523750576b89bbf3b855034413985074
Acondors <alexis06cs@gmail.com>
2023-12-07 16:22:07 -0500

    3 commit

--- anotaciones.php
+++ anotaciones.php
@@ -1,9 +1,34 @@
 <?php
-    require_once("config.php");//Se incorpora el contenido del archivo config.php a este archivo
-    $rs = mysqli_query($cn,"select * from anotaciones");
-    while($row = mysqli_fetch_assoc($rs)){
-        $res[] = array_map("utf8_encode",$row);
+    header("Access-Control-Allow-Origin: *");
+
+    // Intenta conectar a la base de datos
+    $cn = mysqli_connect("localhost", "root", "root", "bdNotas");
+
+    // Verifica si la conexión fue exitosa
+    if (!$cn) {
+        die("Error de conexión: " . mysqli_connect_error());
     }
+
+    // Ejecuta la consulta SQL
+    $rs = mysqli_query($cn, "SELECT * FROM anotaciones");
+
+    // Verifica si la consulta se ejecutó correctamente
+    if (!$rs) {
+        die("Error en la consulta: " . mysqli_error($cn));
+    }
+
+    // Inicializa el array de resultados
+    $res = array();
+
+    // Recorre los resultados y los agrega al array
+    while ($row = mysqli_fetch_assoc($rs)) {
+        // Aplica utf8_encode a cada valor en la fila
+        $res[] = array_map("utf8_encode", $row);
+    }
+
+    // Muestra los datos en formato JSON
     echo json_encode($res);
-    mysqli_close($cn);//Asi se cierra la conexión con la base de datos
+
+    // Cierra la conexión con la base de datos
+    mysqli_close($cn);
 ?>
--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    //Se permite asi el acceso a este servicio desde cualquier dominio
 
-    
-    $cn = mysqli_connect("localhost","root","root6","bdnotas");
+    // Intenta conectar a la base de datos
+    $cn = mysqli_connect("localhost", "root", "root6", "bdnotas");
+
+    // Verifica si la conexión fue exitosa
+    if (!$cn) {
+        die("Error de conexión: " . mysqli_connect_error());
+    }
 ?>
